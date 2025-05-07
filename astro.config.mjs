53054d2526bba1e7417e7400f02d9b85c61f2fcf
Cory Boyd <me@corytheboyd.com>
2025-05-06 18:45:38 -0600

    Initial blog site thing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://blog.corytheboyd.com",
+  integrations: [mdx(), sitemap(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
